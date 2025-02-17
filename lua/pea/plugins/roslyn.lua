0db068745115019e3a7408f5ba6bb050344c639b
CPea <cpea2506@gmail.com>
2025-02-17 12:06:23 +0700

    Fix nvim 0.11 Roslyn issue

--- lua/pea/plugins/roslyn.lua
+++ lua/pea/plugins/roslyn.lua
@@ -21,61 +21,56 @@ return {
 				["csharp|code_lens"] = {
 					dotnet_enable_references_code_lens = true,
 				},
+				["csharp|completion"] = {
+					dotnet_show_completion_items_from_unimported_namespaces = true,
+					dotnet_show_name_completion_suggestions = true,
+				},
+				["csharp|symbol_search"] = {
+					dotnet_search_reference_assemblies = true,
+				},
 			},
 			on_attach = function(client, bufnr)
-				-- Make sure this happens once per client, not per buffer
-				if not client.is_hacked then
-					client.is_hacked = true
+				if client.is_hacked then
+					return
+				end
 
-					-- Let the runtime know the server can do semanticTokens/full now
-					client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
-						semanticTokensProvider = {
-							full = true,
-						},
-					})
+				client.is_hacked = true
 
-					-- Monkey patch the request proxy
-					local request_inner = client.request
-					client.request = function(method, params, handler)
-						if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-							return request_inner(method, params, handler)
-						end
+				-- let the runtime know the server can do semanticTokens/full now
+				client.server_capabilities = vim.tbl_deep_extend("force", client.server_capabilities, {
+					semanticTokensProvider = {
+						full = true,
+					},
+				})
 
-						local function find_buf_by_uri(search_uri)
-							local bufs = vim.api.nvim_list_bufs()
-							for _, buf in ipairs(bufs) do
-								local name = vim.api.nvim_buf_get_name(buf)
-								local uri = "file://" .. name
-								if uri == search_uri then
-									return buf
-								end
-							end
-						end
+				-- monkey patch the request proxy
+				local request_inner = client.request
 
-						local doc_uri = params.textDocument.uri
-						local target_bufnr = find_buf_by_uri(doc_uri)
-						local line_count = vim.api.nvim_buf_line_count(target_bufnr or bufnr)
-						local last_line =
-							vim.api.nvim_buf_get_lines(target_bufnr or bufnr, line_count - 1, line_count, true)[1]
-
-						return request_inner("textDocument/semanticTokens/range", {
-							textDocument = params.textDocument,
-							range = {
-								["start"] = {
-									line = 0,
-									character = 0,
-								},
-								["end"] = {
-									line = line_count - 1,
-									character = string.len(last_line) - 1,
-								},
-							},
-						}, handler)
+				function client:request(method, params, handler, req_bufnr)
+					if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
+						return request_inner(self, method, params, handler)
 					end
+
+					local target_bufnr = vim.uri_to_bufnr(params.textDocument.uri)
+					local line_count = vim.api.nvim_buf_line_count(target_bufnr)
+					local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
+
+					return request_inner(self, "textDocument/semanticTokens/range", {
+						textDocument = params.textDocument,
+						range = {
+							["start"] = {
+								line = 0,
+								character = 0,
+							},
+							["end"] = {
+								line = line_count - 1,
+								character = string.len(last_line) - 1,
+							},
+						},
+					}, handler, req_bufnr)
 				end
 			end,
 		},
-		exe = vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
 		filewatching = true,
 	},
 }
