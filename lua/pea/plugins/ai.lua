8e69ed60c96f3af6bcef4066955254c029be657a
CPea <cpea2506@gmail.com>
2026-03-25 11:01:19 +0700

    Set keymap for applying next edit suggestion

--- lua/pea/plugins/ai.lua
+++ lua/pea/plugins/ai.lua
@@ -3,6 +3,16 @@ return {
     opts = {},
     keys = {
         {
+            "<Tab>",
+            function()
+                if not require("sidekick").nes_jump_or_apply() then
+                    return "<Tab>"
+                end
+            end,
+            expr = true,
+            desc = "Goto/Apply Next Edit Suggestion",
+        },
+        {
             "<leader>ai",
             function()
                 require("sidekick.cli").toggle { name = "copilot", focus = true }
