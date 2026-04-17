b248a666d1c8cf1eb4a2997e07b4fd32e363aa77
CPea <cpea2506@gmail.com>
2026-04-17 16:32:18 +0700

    Telescope for Windows, f*ck it

--- lua/pea/plugins/picker.lua
+++ lua/pea/plugins/picker.lua
@@ -1,85 +1,171 @@
 return {
-    "ibhagwan/fzf-lua",
-    cmd = "FzfLua",
-    keys = {
-        { "<leader>sg", "<cmd>FzfLua global<cr>", desc = "FzfLua Global" },
-        { "<leader>sf", "<cmd>FzfLua files<cr>", desc = "FzfLua Files" },
-        { "<leader>st", "<cmd>FzfLua live_grep<cr>", desc = "FzfLua Grep" },
-        { "<leader>sb", "<cmd>FzfLua buffers<cr>", desc = "FzfLua Buffers" },
-    },
-    init = function(plugin)
-        vim.api.nvim_create_autocmd("User", {
-            group = vim.api.nvim_create_augroup("pea_picker", { clear = true }),
-            pattern = "VeryLazy",
-            callback = function()
-                require(plugin.name).register_ui_select(function(_, items)
-                    local min_height, max_height = 0.15, 0.85
-                    local height = (#items + 4) / vim.o.lines
+    {
+        "ibhagwan/fzf-lua",
+        cmd = "FzfLua",
+        enabled = function()
+            local utils = require "pea.utils"
 
-                    if height < min_height then
-                        height = min_height
-                    elseif height > max_height then
-                        height = max_height
-                    end
+            return not utils.is_windows
+        end,
+        keys = {
+            { "<leader>sg", "<cmd>FzfLua global<cr>", desc = "FzfLua Global" },
+            { "<leader>sf", "<cmd>FzfLua files<cr>", desc = "FzfLua Files" },
+            { "<leader>st", "<cmd>FzfLua live_grep<cr>", desc = "FzfLua Grep" },
+            { "<leader>sb", "<cmd>FzfLua buffers<cr>", desc = "FzfLua Buffers" },
+        },
+        init = function(plugin)
+            vim.api.nvim_create_autocmd("User", {
+                group = vim.api.nvim_create_augroup("pea_picker", { clear = true }),
+                pattern = "VeryLazy",
+                callback = function()
+                    require(plugin.name).register_ui_select(function(_, items)
+                        local min_height, max_height = 0.15, 0.85
+                        local height = (#items + 4) / vim.o.lines
 
-                    return {
-                        winopts = {
-                            height = height,
-                            preview = {
-                                hidden = true,
+                        if height < min_height then
+                            height = min_height
+                        elseif height > max_height then
+                            height = max_height
+                        end
+
+                        return {
+                            winopts = {
+                                height = height,
+                                preview = {
+                                    hidden = true,
+                                },
                             },
-                        },
-                    }
-                end)
-            end,
-        })
-    end,
-    opts = function()
-        local icons = require "pea.ui.icons"
+                        }
+                    end)
+                end,
+            })
+        end,
+        opts = function()
+            local icons = require "pea.ui.icons"
 
-        return {
-            winopts = {
-                height = 0.85,
-                width = 0.5,
-                row = 0.5,
-                col = 0.5,
-                title_flags = false,
-                treesitter = true,
-                preview = {
-                    border = "rounded",
-                    layout = "vertical",
-                    vertical = "up:50%",
-                    scrollbar = false,
+            return {
+                winopts = {
+                    height = 0.85,
+                    width = 0.5,
+                    row = 0.5,
+                    col = 0.5,
+                    title_flags = false,
+                    treesitter = true,
+                    preview = {
+                        border = "rounded",
+                        layout = "vertical",
+                        vertical = "up:50%",
+                        scrollbar = false,
+                    },
                 },
-            },
-            file_ignore_patterns = { "%.meta$" },
-            fzf_opts = {
-                ["--cycle"] = true,
-                ["--gutter"] = " ",
-                ["--no-scrollbar"] = true,
-                ["--pointer"] = icons.ui.ChevronRight,
-                ["--prompt"] = " " .. icons.ui.Telescope .. " ",
-            },
-            fzf_colors = true,
-            files = {
-                cwd_prompt = false,
-                formatter = "path.filename_first",
-            },
-            grep = {
-                hidden = true,
-            },
-            keymap = {
-                builtin = {
-                    true,
-                    ["<C-d>"] = "preview-page-down",
-                    ["<C-u>"] = "preview-page-up",
+                file_ignore_patterns = { "%.meta$" },
+                fzf_opts = {
+                    ["--cycle"] = true,
+                    ["--gutter"] = " ",
+                    ["--no-scrollbar"] = true,
+                    ["--pointer"] = icons.ui.ChevronRight,
+                    ["--prompt"] = " " .. icons.ui.Telescope .. " ",
                 },
-                fzf = {
-                    true,
-                    ["ctrl-d"] = "preview-page-down",
-                    ["ctrl-u"] = "preview-page-up",
+                fzf_colors = true,
+                files = {
+                    cwd_prompt = false,
+                    formatter = "path.filename_first",
                 },
+                grep = {
+                    hidden = true,
+                },
+                keymap = {
+                    builtin = {
+                        true,
+                        ["<C-d>"] = "preview-page-down",
+                        ["<C-u>"] = "preview-page-up",
+                    },
+                    fzf = {
+                        true,
+                        ["ctrl-d"] = "preview-page-down",
+                        ["ctrl-u"] = "preview-page-up",
+                    },
+                },
+            }
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        enabled = function()
+            local utils = require "pea.utils"
+
+            return utils.is_windows
+        end,
+        keys = {
+            { "<leader>sf", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
+            { "<leader>st", "<cmd>Telescope live_grep<cr>", desc = "Telescope Find Text" },
+            { "<leader>sb", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
+        },
+        dependencies = {
+            "nvim-telescope/telescope-ui-select.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
             },
-        }
-    end,
+        },
+        opts = function()
+            local icons = require "pea.ui.icons"
+            local actions = require "telescope.actions"
+
+            return {
+                defaults = {
+                    prompt_prefix = icons.ui.Telescope .. " ",
+                    selection_caret = icons.ui.ChevronRight .. " ",
+                    layout_strategy = "center",
+                    sorting_strategy = "ascending",
+                    path_display = { "smart" },
+                    dynamic_preview_title = true,
+                    results_title = false,
+                    file_ignore_patterns = { "^.git/", "%.meta$" },
+                    layout_config = {
+                        preview_cutoff = 1,
+                        width = function(_, max_columns, _)
+                            return math.min(max_columns, 80)
+                        end,
+                        height = function(_, _, max_lines)
+                            return math.min(max_lines, 15)
+                        end,
+                    },
+                    vimgrep_arguments = {
+                        "rg",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--hidden",
+                        "--glob=!.git/",
+                    },
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                        n = {
+                            ["q"] = actions.close,
+                        },
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        hidden = true,
+                    },
+                },
+            }
+        end,
+        config = function(_, opts)
+            local telescope = require "telescope"
+
+            telescope.setup(opts)
+            telescope.load_extension "fzf"
+            telescope.load_extension "ui-select"
+        end,
+    },
 }
