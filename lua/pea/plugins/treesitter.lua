a44c8e9946ab2623b739ea650c0a68a993aa89a4
CPea <cpea2506@gmail.com>
2026-04-21 11:06:02 +0700

    Replace `nvim-treesitter` with `abrborist`

--- lua/pea/plugins/treesitter.lua
+++ lua/pea/plugins/treesitter.lua
@@ -1,36 +1,10 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
+        "arborist-ts/arborist.nvim",
+        build = ":ArboristUpdate",
         event = "LazyFile",
         dependencies = "nvim-treesitter/nvim-treesitter-context",
-        config = function()
-            local treesitter = require "nvim-treesitter"
-            local parsers = require "nvim-treesitter.parsers"
-
-            vim.api.nvim_create_autocmd("FileType", {
-                group = vim.api.nvim_create_augroup("pea_treesitter", { clear = true }),
-                callback = function(args)
-                    local lang = vim.treesitter.language.get_lang(args.match) or args.match
-
-                    if not parsers[lang] then
-                        return
-                    end
-
-                    local parser = vim.treesitter.get_parser(args.buf, lang, { error = false })
-
-                    if not parser then
-                        treesitter.install(lang):wait(30000)
-                    end
-
-                    vim.treesitter.start(args.buf, lang)
-
-                    if vim.treesitter.query.get(lang, "indents") then
-                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                    end
-                end,
-            })
-        end,
+        opts = {},
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
