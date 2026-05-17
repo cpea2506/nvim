bb39cc3ede29d1aaee92e613870ee5e49df92eb1
CPea <cpea2506@gmail.com>
2026-05-17 13:34:23 +0700

    Set ui2 default target to `msg`

--- lua/pea/ui/init.lua
+++ lua/pea/ui/init.lua
@@ -1,11 +1,7 @@
 require("vim._core.ui2").enable {
     msg = {
         targets = {
-            [""] = "msg",
-            progress = "msg",
-            bufwrite = "msg",
-            undo = "msg",
-            wmsg = "msg",
+            default = "msg",
         },
     },
 }
