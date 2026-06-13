0fbee8fab60f38f372434e02b5f2114dba92005e
CPea <cpea2506@gmail.com>
2026-06-13 20:38:37 +0700

    Re-order events

--- lua/pea/events.lua
+++ lua/pea/events.lua
@@ -1,3 +1,12 @@
+local event_handler = require "lazy.core.handler.event"
+local lazy_file_mapping = {
+    id = "LazyFile",
+    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
+}
+
+event_handler.mappings.LazyFile = lazy_file_mapping
+event_handler.mappings["User LazyFile"] = lazy_file_mapping
+
 vim.on_key(function(char)
     if vim.api.nvim_get_mode().mode ~= "n" then
         return
@@ -11,12 +20,3 @@ vim.on_key(function(char)
         vim.o.hlsearch = should_enable
     end
 end, vim.api.nvim_create_namespace "pea_auto_hlsearch", {})
-
-local event_handler = require "lazy.core.handler.event"
-local lazy_file_mapping = {
-    id = "LazyFile",
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-}
-
-event_handler.mappings.LazyFile = lazy_file_mapping
-event_handler.mappings["User LazyFile"] = lazy_file_mapping
