edc79a24b9eecf9ba400170dce4f5a247c74d5a1
CPea <cpea2506@gmail.com>
2026-07-09 01:52:16 +0700

    Bye `conflict-marker.nvim`

--- lua/pea/plugins/git.lua
+++ lua/pea/plugins/git.lua
@@ -1,119 +1,54 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "LazyFile",
-        opts = {
-            signs = {
-                add = {
-                    text = lib.icons.ui.BoldLine,
-                },
-                change = {
-                    text = lib.icons.ui.BoldLine,
-                },
-                delete = {
-                    text = lib.icons.ui.Triangle,
-                },
-                topdelete = {
-                    text = lib.icons.ui.Triangle,
-                },
-                changedelete = {
-                    text = lib.icons.ui.BoldLine,
-                },
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    opts = {
+        signs = {
+            add = {
+                text = lib.icons.ui.BoldLine,
             },
-            signcolumn = true,
-            numhl = false,
-            linehl = false,
-            word_diff = false,
-            watch_gitdir = {
-                interval = 1000,
-                follow_files = true,
+            change = {
+                text = lib.icons.ui.BoldLine,
             },
-            auto_attach = true,
-            attach_to_untracked = true,
-            current_line_blame = true,
-            current_line_blame_opts = {
-                virt_text = true,
-                virt_text_pos = "eol",
-                delay = 500,
-                ignore_whitespace = false,
+            delete = {
+                text = lib.icons.ui.Triangle,
             },
-            current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y> (<author_time:%R>) "
-                .. lib.icons.ui.CircleMedium
-                .. " <summary>",
-            sign_priority = 6,
-            status_formatter = nil,
-            update_debounce = 100,
-            max_file_length = 40000,
-            preview_config = {
-                border = "rounded",
-                style = "minimal",
-                relative = "cursor",
-                row = 0,
-                col = 1,
+            topdelete = {
+                text = lib.icons.ui.Triangle,
+            },
+            changedelete = {
+                text = lib.icons.ui.BoldLine,
             },
         },
-    },
-    {
-        "tronikelis/conflict-marker.nvim",
-        event = "LazyFile",
-        opts = {
-            highlight = true,
-            on_attach = function(conflict)
-                local mid = "^=======$"
-
-                lib.set_keymaps {
-                    {
-                        "n",
-                        "<C-j>",
-                        function()
-                            vim.cmd("/" .. mid)
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                    {
-                        "n",
-                        "<C-k>",
-                        function()
-                            vim.cmd("?" .. mid)
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                    {
-                        "n",
-                        "co",
-                        function()
-                            conflict:choose_ours()
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                    {
-                        "n",
-                        "ct",
-                        function()
-                            conflict:choose_theirs()
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                    {
-                        "n",
-                        "cb",
-                        function()
-                            conflict:choose_both()
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                    {
-                        "n",
-                        "cn",
-                        function()
-                            conflict:choose_none()
-                        end,
-                        { buf = conflict.bufnr },
-                    },
-                }
-
-                vim.diagnostic.enable(false, { bufnr = conflict.bufnr })
-            end,
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
+        watch_gitdir = {
+            interval = 1000,
+            follow_files = true,
+        },
+        auto_attach = true,
+        attach_to_untracked = true,
+        current_line_blame = true,
+        current_line_blame_opts = {
+            virt_text = true,
+            virt_text_pos = "eol",
+            delay = 500,
+            ignore_whitespace = false,
+        },
+        current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y> (<author_time:%R>) "
+            .. lib.icons.ui.CircleMedium
+            .. " <summary>",
+        sign_priority = 6,
+        status_formatter = nil,
+        update_debounce = 100,
+        max_file_length = 40000,
+        preview_config = {
+            border = "rounded",
+            style = "minimal",
+            relative = "cursor",
+            row = 0,
+            col = 1,
         },
     },
 }
