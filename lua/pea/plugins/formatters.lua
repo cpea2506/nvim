2c5cd5f2fbb23ef32717e27d42f1597bbfb5be44
CPea <cpea2506@gmail.com>
2026-07-09 21:23:24 +0700

    Cleanup confirm.nvim

--- lua/pea/plugins/formatters.lua
+++ lua/pea/plugins/formatters.lua
@@ -2,9 +2,6 @@ return {
     "stevearc/conform.nvim",
     event = "BufWritePre",
     cmd = "ConformInfo",
-    init = function()
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
     opts = {
         formatters_by_ft = {
             json = { "prettier" },
