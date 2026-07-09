b8672080675a27f70f608cfa4df3c721a7de5012
CPea <cpea2506@gmail.com>
2026-07-09 22:00:47 +0700

    Use `vim.fs.ext` for checking file extension

--- after/lsp/shaderls.lua
+++ after/lsp/shaderls.lua
@@ -2,7 +2,7 @@ return {
     cmd = { "shader-ls", "stdio" },
     filetypes = { "glsl", "shaderlab" },
     root_dir = vim.fs.root(0, function(name, _)
-        return name:match "%.csproj$" ~= nil
+        return vim.fs.ext(name) == "csproj"
     end),
     settings = {
         ShaderLab = {
