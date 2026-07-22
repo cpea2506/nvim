65f1a7dc6fcbe2ec9f07dcc8890aa33a1df36ceb
CPea <cpea2506@gmail.com>
2026-07-22 11:21:56 +0700

    Add `lib.set_keymap` and `lib.create_autocmd`

--- lua/pea/lib/init.lua
+++ lua/pea/lib/init.lua
@@ -1,43 +1,43 @@
 local M = {}
 
-local on = require("vim._core.util").nvim_on
-
 ---List of icons.
 M.icons = require "pea.lib.icons"
 
 ---Whether current OS is Windows.
 M.is_windows = jit.os:find "Windows" ~= nil
 
----@class Autocmd
----@field [1] vim.api.keyset.events|vim.api.keyset.events[]
----@field [2] string|integer?
----@field [3] vim.api.keyset.create_autocmd|fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?
----@field [4]? fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?
+---@alias Lib.Autocmd
+--- | [vim.api.keyset.events|vim.api.keyset.events[], string|integer?, vim.api.keyset.create_autocmd, fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?]
+--- | [vim.api.keyset.events|vim.api.keyset.events[], string|integer?, fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?]
+
+M.create_autocmd = require("vim._core.util").nvim_on
 
 ---Create autocmds.
----@param autocmds Autocmd[] #List of autocmds.
+---@param autocmds Lib.Autocmd[] #List of autocmds.
 function M.create_autocmds(autocmds)
     for _, autocmd in ipairs(autocmds) do
         local events, group, opts_or_fn, fn = unpack(autocmd)
 
-        on(events, group, opts_or_fn, fn)
+        if type(opts_or_fn) == "function" then
+            M.create_autocmd(events, group, opts_or_fn)
+        else
+            M.create_autocmd(events, group, opts_or_fn, fn)
+        end
     end
 end
 
----@class Keymap
----@field [1] string|string[]
----@field [2] string
----@field [3] string|function
----@field [4]? vim.keymap.set.Opts
+M.set_keymap = vim.keymap.set
+
+---@alias Lib.Keymap [string|string[], string, string|function, vim.keymap.set.Opts]
 
 ---Set keymaps.
----@param keymaps Keymap[] #List of keymaps.
+---@param keymaps Lib.Keymap[] #List of keymaps.
 function M.set_keymaps(keymaps)
     for _, keymap in ipairs(keymaps) do
         local modes, lhs, rhs, opts = unpack(keymap)
         opts = vim.tbl_extend("force", { silent = true }, opts or {})
 
-        vim.keymap.set(modes, lhs, rhs, opts)
+        M.set_keymap(modes, lhs, rhs, opts)
     end
 end
 
