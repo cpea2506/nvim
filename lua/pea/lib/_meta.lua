717d373e9882f51ad8bdeccb828f8052690a1d14
CPea <cpea2506@gmail.com>
2026-07-22 20:51:32 +0700

    Correct `Lib.Keymap` type alias

--- lua/pea/lib/_meta.lua
+++ lua/pea/lib/_meta.lua
@@ -5,4 +5,4 @@ error "Cannot require a meta file"
 --- | [vim.api.keyset.events|vim.api.keyset.events[], string|integer?, vim.api.keyset.create_autocmd, fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?]
 --- | [vim.api.keyset.events|vim.api.keyset.events[], string|integer?, fun(ev: vim.api.keyset.create_autocmd.callback_args): boolean?]
 
----@alias Lib.Keymap [string|string[], string, string|function, vim.keymap.set.Opts]
+---@alias Lib.Keymap [string|string[], string|string[], function|string, vim.keymap.set.Opts?]
