cadde899fe46ce3b0ad4833308cc90eaa02136b0
CPea <cpea2506@gmail.com>
2026-07-28 10:04:51 +0700

    Add more useful global keymaps

--- lua/pea/keymaps.lua
+++ lua/pea/keymaps.lua
@@ -1,13 +1,20 @@
 lib.set_keymaps {
-    { "n", "<leader>q", "<cmd>q<cr>" },
-    { "n", "<C-s>", "<cmd>w<cr>" },
-    { "n", "<C-e>", "<cmd>bd<cr>" },
+    { "i", "<C-h>", "<Left>", { desc = "Move left" } },
+    { "i", "<C-l>", "<Right>", { desc = "Move right" } },
+    { "i", "<C-j>", "<Down>", { desc = "Move down" } },
+    { "i", "<C-k>", "<Up>", { desc = "Move up" } },
 
-    { "n", "<C-h>", "<C-w>h" },
-    { "n", "<C-j>", "<C-w>j" },
-    { "n", "<C-k>", "<C-w>k" },
-    { "n", "<C-l>", "<C-w>l" },
+    { "n", "<C-h>", "<C-w>h", { desc = "Switch window left" } },
+    { "n", "<C-j>", "<C-w>j", { desc = "Switch window down" } },
+    { "n", "<C-k>", "<C-w>k", { desc = "Switch window up" } },
+    { "n", "<C-l>", "<C-w>l", { desc = "Switch window right" } },
 
-    { "v", "<", "<gv" },
+    { "n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" } },
+    { "n", "<C-e>", "<cmd>bd<cr>", { desc = "Close buffer" } },
+
+    { "n", "<leader>w", "<cmd>noautocmd w<cr>", { desc = "Save without autocmd" } },
+    { "n", "<C-s>", "<cmd>w<cr>", { desc = "Save" } },
+
+    { "v", "<", "<gv", { desc = "Move text" } },
     { "v", ">", ">gv" },
 }
