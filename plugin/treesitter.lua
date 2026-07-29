ba894bf11d8a043245614632ccd1c6b6dcecf24b
CPea <cpea2506@gmail.com>
2026-07-29 20:58:52 +0700

    Load treesitter correctly on reading buffer

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,39 +1,44 @@
-lib.create_autocmd("FileType", vim.api.nvim_create_augroup("pea_plugin", { clear = false }), { once = true }, function()
-    vim.pack.add {
-        "https://github.com/nvim-treesitter/nvim-treesitter",
-        "https://github.com/nvim-treesitter/nvim-treesitter-context",
-    }
+lib.create_autocmd(
+    { "BufReadPre", "BufNewFile" },
+    vim.api.nvim_create_augroup("pea_plugin", { clear = false }),
+    { once = true },
+    function()
+        vim.pack.add {
+            "https://github.com/nvim-treesitter/nvim-treesitter",
+            "https://github.com/nvim-treesitter/nvim-treesitter-context",
+        }
 
-    local treesitter = require "nvim-treesitter"
-    local parsers = require "nvim-treesitter.parsers"
+        require("treesitter-context").setup {
+            mode = "cursor",
+            max_lines = 3,
+        }
 
-    lib.create_autocmd("FileType", vim.api.nvim_create_augroup "pea_treesitter", function(args)
-        local buf = args.buf
-        local lang = vim.treesitter.language.get_lang(args.match) or args.match
+        lib.set_keymap("n", "[c", function()
+            require("treesitter-context").go_to_context(vim.v.count1)
+        end, { desc = "Go To Context" })
 
-        if not parsers[lang] then
-            return
-        end
+        lib.create_autocmd("FileType", vim.api.nvim_create_augroup "pea_treesitter", function(args)
+            local buf = args.buf
+            local lang = vim.treesitter.language.get_lang(args.match) or args.match
 
-        local parser = vim.treesitter.get_parser(buf, lang, { error = false })
+            local treesitter = require "nvim-treesitter"
+            local parsers = require "nvim-treesitter.parsers"
 
-        if not parser then
-            treesitter.install(lang):wait(30000)
-        end
+            if not parsers[lang] then
+                return
+            end
 
-        vim.treesitter.start(buf, lang)
+            local parser = vim.treesitter.get_parser(buf, lang, { error = false })
 
-        if vim.treesitter.query.get(lang, "indents") then
-            vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-    end)
+            if not parser then
+                treesitter.install(lang):wait(30000)
+            end
 
-    require("treesitter-context").setup {
-        mode = "cursor",
-        max_lines = 3,
-    }
+            vim.treesitter.start(buf, lang)
 
-    lib.set_keymap("n", "[c", function()
-        require("treesitter-context").go_to_context(vim.v.count1)
-    end, { desc = "Go To Context" })
-end)
+            if vim.treesitter.query.get(lang, "indents") then
+                vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
+        end)
+    end
+)
