17346b7c4d7bbe60997b929bf01d241787b03404
CPea <cpea2506@gmail.com>
2026-09-08 23:33:35 +0700

    Move `blink.pairs` to editor module

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -1,52 +1,6 @@
 local augroup = vim.api.nvim_create_augroup("pea_plugin", { clear = false })
 
-vim.pack.add({
-    "https://github.com/nvim-tree/nvim-web-devicons",
-    "https://github.com/saghen/blink.lib",
-    "https://github.com/saghen/blink.pairs",
-    "https://github.com/rafamadriz/friendly-snippets",
-}, { load = false })
-
 lib.create_autocmds {
-    {
-        "PackChanged",
-        augroup,
-        function(args)
-            local name, kind = args.data.spec.name, args.data.kind
-
-            if name == "blink.pairs" and (kind == "install" or kind == "update") then
-                if not args.data.active then
-                    vim.cmd.packadd "blink.lib"
-                    vim.cmd.packadd "blink.pairs"
-                end
-
-                require("blink.pairs").build():pwait()
-            end
-        end,
-    },
-    {
-        { "BufReadPost", "BufNewFile" },
-        augroup,
-        { once = true },
-        function()
-            vim.cmd.packadd "blink.lib"
-            vim.cmd.packadd "blink.pairs"
-
-            require("blink.pairs").setup {
-                highlights = {
-                    groups = {
-                        "RainbowDelimiterRed",
-                        "RainbowDelimiterOrange",
-                        "RainbowDelimiterYellow",
-                        "RainbowDelimiterGreen",
-                        "RainbowDelimiterBlue",
-                        "RainbowDelimiterViolet",
-                        "RainbowDelimiterCyan",
-                    },
-                },
-            }
-        end,
-    },
     {
         "CmdlineChanged",
         augroup,
